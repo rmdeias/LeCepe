8d639bebd919eadb1fda9d662780e3c196916d4c
remi deias <rmdeias@gmail.com>
2022-01-29 17:55:56 +0100

    product page sql request for more products

--- Front/Controllers/Product.php
+++ Front/Controllers/Product.php
@@ -7,9 +7,12 @@ if(isset($_GET["le_cepe_records_merch"])) {
    
     $id = explode("-",$_GET["le_cepe_records_merch"]);
     
-    $moreProducts = new ReadDeleteManager('products');
-    $product = $moreProducts->readInnerJoinWhereProductId('products.*,name,bandSlug',$id[1]);
-   
+    $products = new ReadDeleteManager('products');
+    $product = $products->readInnerJoinWhereProductId('products.*,name,bandSlug',$id[1]);
+     
+    $moreProductsFromThisArtist = new ReadDeleteManager('products');
+    $moreProducts = $moreProductsFromThisArtist -> readInnerJoinAllReleases('products.*,name,bandSlug','WHERE products.id != '.$id[1].' AND products.id_band ='.$id[0]);
+
     $title = $product["name"]." ".$product["title"];
     require "./Front/Views/Product.phtml";
     require "./Front/Views/Layout.phtml";  

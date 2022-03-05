af3ee8493d3913e8adb2eb84d51e7a4151715e4d
remi deias <rmdeias@gmail.com>
2022-03-05 17:21:33 +0100

    if artist has just one product, merchandising product replace more product from this artist

--- Front/Controllers/Product.php
+++ Front/Controllers/Product.php
@@ -9,10 +9,14 @@ if(isset($_GET["le_cepe_records_merch"])) {
     
     $products = new ReadDeleteManager('products');
     $product = $products->readInnerJoinWhereProductId('products.*,name,bandSlug',$id[1]);
-     
+   
     $moreProductsFromThisArtist = new ReadDeleteManager('products');
     $moreProducts = $moreProductsFromThisArtist -> readInnerJoinAllReleases('products.*,name,bandSlug','WHERE products.id != '.$id[1].' AND products.id_band ='.$id[0]);
 
+    // if artist have only one product
+    $MerchProducts = new ReadDeleteManager('products');
+    $merchs = $MerchProducts->readInnerJoinAllReleases('products.*,name,bandSlug',"WHERE products.dispo = 'New' AND name = 'Le Cepe Records Merch' limit 4");
+    
     $title = $product["name"]." ".$product["title"];
     require "./Front/Views/Product.phtml";
     require "./Front/Views/Layout.phtml";  
